b1c223d929493ec5a894d719bcd7798a2d94866e
Saverio <SaCut@users.noreply.github.com>
2021-05-10 23:11:32 +0100

    updated README.md, added contents and functionality to main.tf

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
-# terraform
+# Terraform
+- Terraform is an orchestration tool for building, changing, and versioning infrastructure.
+- It allows treating infosrtucture (servers, VPCs, subnets) as pieces of code on a file.
+- Terraform uses json-style syntax for its input files and a specific file extension, `.tf`.
+- Compared to Ansible, Terraform is of much easier use.
 
-### Resources
-#### EC2 instance AMI IDs:
-- Webapp AMI ID: ami-040b1f6b252d7350d
-- Database AMI ID: ami-04408febde5e989a1
+#### Terraform diagram
+![img](https://imgur.com/SgBFr0Z.png)
 
 #### Credentials
 - Create two environment variables, one called `AWS_ACCESS_KEY_ID`, another called `AWS_SECRET_ACCESS_KEY`, and set them to the AWS access credentials
 - edit `nano main.tf`
-- choose provider with
+
+#### useful commands
+- `terraform init` initialises the terraform project
+- `terraform plan` checks the syntax of the code and that the project has no running problems
+- `terraform appy` actually creates the objects specified in the present-path file
+- `terraform destroy` destroys all the objects specified in the present-path file
+
+#### Creating objects - example of a file
+- A cloud service provider can be choosen with
 ```tf
 provider "aws" {
   # define the availability region for the instance
   region = "eu-west-1"
 }
 ```
-- Set up instance:
+- Setting up an instance:
 ```tf
 resource "aws_instance" "app_instance" {
   #add the AMI between double quotes
@@ -33,9 +43,3 @@ resource "aws_instance" "app_instance" {
   }
 }
 ```
-
-#### useful commands
-- `terraform init` initialises the terraform project
-- `terraform plan` checks the syntax of the code and that the project has no running problems
-- `terraform appy`
-- `terraform destroy`
--- main.tf
+++ main.tf
@@ -4,28 +4,140 @@
 
 # This code will eventually launch an EC2 instance for us
 
-# provider is a keyword to define the cloud provider
 
-provider "aws" {
-  # define the availability region for the instance
-  region = "eu-west-1"
+# ----- VARIABLES -----
+
+# variable "subnets_cidr" {
+#   type = list
+#   default = ["10.0.1.0/24", "10.0.2.0/24"]
+# }
+
+locals {
+  az = "eu-west-1a"
+  app_image = "ami-040b1f6b252d7350d" # app instance image
+  db_image = "ami-04408febde5e989a1" # database instance id
+  type = "t2.micro" # defines the type of instance
+  key = "eng84devops" # defines the ssh key to be used
 }
 
-# launching an EC2 instance from an AMI
-# resource allows us to add aws
 
-resource "aws_instance" "app_instance" {
-  #add the AMI between double quotes
-  ami = "ami-040b1f6b252d7350d"
+# ----- DEFINE PROVIDER -----
+provider "aws" { # provider is a keyword to define the cloud provider
+  region = local.az # define the availability region for the instance
+}
 
-  # let's also define the type of instance
-  instance_type = "t2.micro"
 
-  # enable piblic ip on instance
-  associate_public_ip_address = true
+# ----- CREATE RESOURCES -----
 
-  # also add tags
+# ----- VPC RESOURCES -----
+
+# block of code to create a VPC
+resource "aws_vpc" "sav_tf_vpc" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+
   tags = {
-      Name = "eng84_saverio_tf_node_app"
+    Name = "eng84_saverio_tf_vpc"
+  }
+}
+
+# create internet gateway
+resource "aws_internet_gateway" "sav_tf_gate" {
+  vpc_id = aws_vpc.sav_tf_vpc.id
+
+  tags = {
+    name = "eng84_sav_tf_gateway"
+  }
+}
+
+# create route table
+resource "aws_route_table" "sav_tf_route" {
+  vpc_id = aws_vpc.sav_tf_vpc.id
+  # subnet_id = aws_subnet.sav_public_net.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.sav_tf_gate.id
+  }
+
+  tags = {
+    name = "eng84_sav_tf_public_RT"
+  }
+}
+
+# block of code to create a public subnet
+resource "aws_subnet" "sav_public_net" {
+  vpc_id = aws_vpc.sav_tf_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true" # makes it a public subnet
+  availability_zone = local.az
+  # route_table = aws_route_table.sav_tf_route.id
+
+  tags = {
+    name = "eng84_sav_tf_public_net"
+  }
+}
+
+# block of code to create a private subnet
+resource "aws_subnet" "sav_private_net" {
+  vpc_id = aws_vpc.sav_tf_vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "false" # makes it a private subnet
+
+  tags = {
+    name = "eng84_sav_tf_private_net"
+  }
+}
+
+# route table association to public subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.sav_public_net.id
+  route_table_id = aws_route_table.sav_tf_route.id
+}
+
+# route table association to public subnet ---- TEMP ----
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.sav_private_net.id
+  route_table_id = aws_route_table.sav_tf_route.id # associated with public route table for debug
+}
+
+
+# ----- EC2 RESOURCES -----
+
+# launching app EC2 instance from AMI
+resource "aws_instance" "sav_tf_app" {
+  ami = local.app_image # define the source image
+
+  instance_type = local.type # define the type of instance
+
+  key_name = local.key
+
+  private_ip = "10.0.1.100" # set the private ip
+
+  associate_public_ip_address = true # enable public ip on instance
+
+  subnet_id = aws_subnet.sav_public_net.id # set the subnet
+
+  tags = {
+      Name = "eng84_sav_tf_app"
+  }
+}
+
+# launching db EC2 instance from AMI
+resource "aws_instance" "sav_tf_db" {
+  ami = local.db_image # define the source image
+
+  instance_type = local.type
+
+  key_name = local.key
+
+  private_ip = "10.0.2.100" # set the private ip
+
+  associate_public_ip_address = false # no public ip
+
+  subnet_id = aws_subnet.sav_private_net.id
+
+  tags = {
+      Name = "eng84_sav_tf_app"
   }
 }
